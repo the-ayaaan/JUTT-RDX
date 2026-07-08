31e649005cb6d6cab5e2836a2472832f19920dd0
ali661g44-stack <ali661g44@gmail.com>
2026-07-08 11:32:36 +0500

    Update bot.js

--- modules/commands/bot.js
+++ modules/commands/bot.js
@@ -44,7 +44,7 @@ function loadReplies() {
 function pickReply({ senderID, gender }) {
   const replies = loadReplies();
   let category = "default";
-  if (senderID === "61591807887778") category = "61591807887778";
+  if (senderID === "61591533960444") category = "61591533960444";
   else if (gender === 2 || gender?.toString().toUpperCase() === "MALE") category = "MALE";
   else if (gender === 1 || gender?.toString().toUpperCase() === "FEMALE") category = "FEMALE";
 
